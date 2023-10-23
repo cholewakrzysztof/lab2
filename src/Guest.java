2e1487ae66b895a51b5e2df1fa9bb0302f6054fa
cholewakrzysztof <cholewa.krzysztof24@gmail.com>
2023-10-23 13:40:48 +0200

    Add first solution of resolve

--- src/Guest.java
+++ src/Guest.java
@@ -1,19 +1,32 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-public class Guest {
+public class Guest{
     Map<Attribute,Float> ownAttribute;
     Map<Attribute,Float> searchAttribute;
     Integer assignmentNumber;
+
     List<Guest> assignedGuests;
+    List<Integer> assignedGuestsIDs;
+    private Float B = 0.0f;
+    private static Integer static_ID = 0;
+    public Integer ID;
     public Guest(Map<Attribute,Float> ownAttribute, Map<Attribute,Float> searchAttribute){
         this.ownAttribute = ownAttribute;
         this.searchAttribute = searchAttribute;
         assignedGuests = new LinkedList<>();
+        assignedGuestsIDs = new LinkedList<>();
         assignmentNumber = 0;
+        this.ID = static_ID;
+        static_ID++;
     }
-
+    public Guest(Guest g){
+        this.ownAttribute = g.ownAttribute;
+        this.searchAttribute = g.searchAttribute;
+        assignedGuests = g.assignedGuests;
+        assignmentNumber = g.assignmentNumber;
+        assignedGuestsIDs = g.assignedGuestsIDs;
+        this.ID = g.ID;
+    }
     public String toString(){
         String result = "";
         for (Attribute atr: ownAttribute.keySet()) {
@@ -23,6 +36,55 @@ public class Guest {
         for (Attribute atr: searchAttribute.keySet()) {
             result+=atr.toString()+":"+ searchAttribute.get(atr).toString()+",";
         }
+        return result;
+    }
+
+    public Float countScore(Guest otherGuest){
+        Float score = 0f;
+        for (Attribute atr: this.ownAttribute.keySet()) {
+            if (otherGuest.searchAttribute.keySet().contains(atr)){
+                score+= ownAttribute.get(atr)*otherGuest.searchAttribute.get(atr);
+            }
+        }
+        for (Attribute atr: this.searchAttribute.keySet()){
+            if(otherGuest.ownAttribute.keySet().contains(atr)){
+                score+= searchAttribute.get(atr)*otherGuest.ownAttribute.get(atr);
+            }
+        }
+        score -= otherGuest.assignmentNumber*B;
+
+        return score;
+    }
+
+    public void sortAssignedGuests(){
+        for(int i=0; i< assignedGuests.size()-1; i++){
+            for(int j=0; j< assignedGuests.size()-1; j++){
+                Float g1Score = this.countScore(assignedGuests.get(j));
+                Float g2Score = this.countScore(assignedGuests.get(j+1));
+                if(g1Score<g2Score){
+                    Guest tmp = new Guest(assignedGuests.get(j+1));
+                    assignedGuests.set(j+1,new Guest(assignedGuests.get(j)));
+                    assignedGuests.set(j,tmp);
+
+                    Integer tmp2 = assignedGuestsIDs.get(j+1);
+                    assignedGuestsIDs.set(j+1,assignedGuests.get(j).ID);
+                    assignedGuestsIDs.set(j,tmp2);
+                }
+            }
+        }
+        if(assignedGuests.size()>5){
+            assignedGuests.get(5).assignmentNumber--;
+            assignedGuests = assignedGuests.subList(0,5);
+            assignedGuestsIDs = assignedGuestsIDs.subList(0,5);
+        }
+    }
+
+    public String printAssignedGuests(){
+        String result = "For guest number: "+this.ID+" = {";
+        for (Guest g:assignedGuests) {
+            result += g.ID+" "+this.countScore(g)+",";
+        }
+        result = result.substring(0,result.length()-1)+"}";
         return result;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,7 +7,7 @@ public class Main {
         List<Guest> guests = new LinkedList<>();
         Random random = new Random();
 
-        for (int i= 0; i<2; i++){
+        for (int i= 0; i<40; i++){
             guests.add(GuestFactory.createGuest(random.nextInt(1,3),random.nextInt(1,5)));
         }
 
@@ -24,5 +22,24 @@ public class Main {
             System.out.println(g.toString());
         }
 
+        System.out.println(guests.get(0).countScore(guests.get(1)));
+        System.out.println(guests.get(1).countScore(guests.get(0)));
+
+        for (Guest guest : guests) {
+            for (Guest possibleMatch : guests) {
+                if (Objects.equals(guest.ID, possibleMatch.ID))
+                    continue;
+                if (guest.assignedGuestsIDs.contains(possibleMatch.ID))
+                    continue;
+                possibleMatch.assignmentNumber++;
+                guest.assignedGuests.add(possibleMatch);
+                guest.assignedGuestsIDs.add(possibleMatch.ID);
+                guest.sortAssignedGuests();
+            }
+        }
+
+        for (Guest guest:guests) {
+            System.out.println(guest.printAssignedGuests());
+        }
     }
 }
